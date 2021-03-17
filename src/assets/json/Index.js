3dfa5ae338b0b6d19497fae206a5bf208024b3b9
abuazis <abuaziscorp@gmail.com>
2021-03-17 13:30:44 +0700

    Fix Identation & Prettier

--- src/assets/json/Index.js
+++
@@ -1,5 +0,0 @@
-import workingSpaceData from './working_space_data.json';
-import monthData from './month_data.json';
-import typeRoomData from './type_room_data.json';
-
-export { workingSpaceData, monthData, typeRoomData };
